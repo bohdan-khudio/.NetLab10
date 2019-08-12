5c3f6995d89b14bd0f08ecf01c711ec799b5c31d
bohdan-khudio <bahdan510@gmail.com>
2019-08-13 01:52:09 +0300

    Добавьте файлы проекта.

---
+++ dotNetLab10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotNetLab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
