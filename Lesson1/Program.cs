3e106723d4c2752484603a797321e45ca5e559f3
bohdan-khudio <bahdan510@gmail.com>
2019-08-13 04:09:47 +0300

    Lesson 1 done

---
+++ Lesson1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Lesson1.Tasks;
+using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1 task1 = new Task1();
+            //task1.Run();
+
+            //Task2 task2 = new Task2();
+            //task2.Run();
+
+            //Task3 task3 = new Task3();
+            //task3.Run();
+
+            //Task4 task4 = new Task4();
+            //task4.Run();
+
+            Task5 task5 = new Task5();
+            task5.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson1/Tasks/Task3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson1.Tasks
+{
+    public class Circle
+    {
+        private int r;
+        private const double pi = 3.14159265358979;
+
+        public Circle(int r)
+        {
+            this.r = r;
+        }
+
+
+        public double Leght()
+        {
+            return 2 * pi * r;
+        }
+
+        public double Square()
+        {
+            return pi * r * r;
+        }
+    }
+
+    public class Task3
+    {
+        public void Run()
+        {
+            Console.WriteLine("Task 3 \nInput radus");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Circle cir = new Circle(r);
+            Console.WriteLine($"Square = {cir.Square()}, Leght = {cir.Leght()}");
+        }
+    }
+}
